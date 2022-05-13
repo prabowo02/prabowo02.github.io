30cddb3b6566fd990c572e9ca19cbe77bd8c9603
prabowo02 <prabowo1048576@gmail.com>
2022-05-13 18:41:37 +0000

    Squash covid data

--- covid/update.sh
+++ covid/update.sh
@@ -1,7 +1,7 @@
+cd $(dirname "$0")
 rm owid-covid-data.csv
 wget https://covid.ourworldindata.org/data/owid-covid-data.csv
 python3 parse.py
-git add .
-git commit -m "Update covid data"
-git push origin master
-
+# git add .
+# git commit -m "Update covid data"
+# git push origin master
